5dc78fcc0d9710ece0e80167af23d391b0052c65
lleballex <lleballex@yandex.ru>
2023-05-08 12:49:30 +0300

    Fix

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import math
 import customtkinter as ctk
 
 
+description = ('Программа найдет кратчайшие пути в графе по алгоритму Флойда\n'
+               'Обозначения:\n'
+               'Путь из вершины в саму себя - X\n'
+               'Отсутствие пути (∞) - пустое поле или inf')
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry('500x500')
-        self.title('Алгорти Флойда')
+        self.geometry('600x600')
+        self.title('Алгортм Флойда')
         self.grid_columnconfigure(0, weight=1)
-        # self.grid_rowconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.size_frame = ctk.CTkFrame(self, fg_color='transparent',)
-        self.size_frame.grid(row=0, column=0, padx=10, pady=10, sticky='n')
+        self.main_frame = ctk.CTkScrollableFrame(self, fg_color='transparent')
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.main_frame.grid(row=0, column=0, sticky='news')
+
+        self.description = ctk.CTkLabel(self.main_frame, text=description)
+        self.description.grid(row=0, column=0, pady=10)
+
+        self.size_frame = ctk.CTkFrame(self.main_frame, fg_color='transparent',)
+        self.size_frame.grid(row=1, column=0, padx=10, pady=10, sticky='n')
 
         self.size_entry = ctk.CTkEntry(self.size_frame, placeholder_text='Количество вершин')
         self.size_entry.grid(row=0, column=0, padx=(0, 10))
@@ -20,14 +33,14 @@ class App(ctk.CTk):
         self.size_button = ctk.CTkButton(self.size_frame, text='Создать матрицу', command=self.create_matrix)
         self.size_button.grid(row=0, column=1)
 
-        self.matrix_frame = ctk.CTkFrame(self)
-        self.matrix_frame.grid(row=1, column=0, sticky='s')
+        self.matrix_frame = ctk.CTkFrame(self.main_frame)
+        self.matrix_frame.grid(row=2, column=0, sticky='s')
 
-        self.submit_btn = ctk.CTkButton(self, text='Рассчитать по алгоритму Флойда', command=self.floyd)
-        self.submit_btn.grid(row=2, column=0, pady=10)
+        self.submit_btn = ctk.CTkButton(self.main_frame, text='Рассчитать по алгоритму Флойда', command=self.floyd)
+        self.submit_btn.grid(row=3, column=0, pady=10)
 
-        self.output_matrix_frame = ctk.CTkFrame(self)
-        self.output_matrix_frame.grid(row=3, column=0)
+        self.output_matrix_frame = ctk.CTkFrame(self.main_frame)
+        self.output_matrix_frame.grid(row=4, column=0)
 
         self.matrix_size = 0
         self.prev_matrix_size = 0
@@ -46,11 +59,18 @@ class App(ctk.CTk):
         for i in range(self.matrix_size):
             self.matrix.append([])
             for j in range(self.matrix_size):
-                self.matrix[i].append(ctk.CTkEntry(self.matrix_frame, width=40))
+                self.matrix[i].append(ctk.CTkEntry(self.matrix_frame, width=35))
                 if i == j: self.matrix[i][j].insert(0, 'X')
                 self.matrix[i][j].grid(row=i, column=j, padx=5, pady=5)
 
     def floyd(self):
+        for i in range(self.prev_matrix_size):
+            for j in range(self.prev_matrix_size):
+                self.output_matrix[i][j].destroy()
+                
+        self.output_matrix = []
+        self.prev_matrix_size = 0
+
         matrix = []
 
         for i in range(self.matrix_size):
@@ -62,26 +82,18 @@ class App(ctk.CTk):
                 elif val.lower() == 'inf' or not val:
                     matrix[i].append(math.inf)
                 else:
-                    matrix[i].append(int(val))
+                    if (val.isnumeric()):
+                        matrix[i].append(int(val))
+                    else:
+                        self.matrix[i][j].focus() 
+                        return
 
-        # --- floyd
-
-
-        print(matrix)
         for k in range(self.matrix_size):
             prev = matrix.copy()
             for i in range(self.matrix_size):
                 for j in range(self.matrix_size):
                     matrix[i][j] = min(prev[i][j], prev[i][k] + prev[k][j])
-            print(matrix)
 
-        # --- floyd
-
-        for i in range(self.prev_matrix_size):
-            for j in range(self.prev_matrix_size):
-                self.output_matrix[i][j].destroy()
-                
-        self.output_matrix = []
         self.prev_matrix_size = self.matrix_size
 
         for i in range(self.matrix_size):
@@ -92,7 +104,6 @@ class App(ctk.CTk):
                 elif text == math.inf: text = '∞'
                 self.output_matrix[i].append(ctk.CTkLabel(self.output_matrix_frame, text=text))
                 self.output_matrix[i][j].grid(row=i, column=j, padx=7)
-
 
 
 if __name__ == '__main__':
